6b019c95d7246be271b697047b06ce0e66caca3f
Vladimir Djurovic <vdjurovic@vektorsoft.com>
2022-05-31 12:26:49 +0200

    initial change

---
+++ src/test/kotlin/co/bitshifted/backstage/BackstageApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.bitshifted.backstage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackstageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
